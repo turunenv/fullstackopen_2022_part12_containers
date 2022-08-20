efbbd2799ad87c5eabd7ae76d70d9d5bf8ad5185
turunenv <vili.turunen@aalto.fi>
2022-08-20 12:07:09 +0300

    12.10 - initialize redis at startup by getting the todo-count from mongo

--- part12-containers-applications/todo-app/todo-backend/app.js
+++ part12-containers-applications/todo-app/todo-backend/app.js
@@ -3,8 +3,18 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
+
+
 const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/todos');
+const statisticsRouter = require('./routes/statistics');
+
+//initialize the redis-counter from the database
+const { Todo } = require('./mongo');
+const { setAsync } = require('./redis/index')
+Todo.find({}).then(todos => {
+  setAsync('added_todos', todos.length)
+})
 
 const app = express();
 
@@ -15,5 +25,6 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/todos', todosRouter);
+app.use('/statistics', statisticsRouter);
 
 module.exports = app;
